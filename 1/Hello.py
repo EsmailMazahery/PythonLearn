df54850c033677d12d1602da677a7b748beac513
Esmaeil mazahery <smaeil.mazahery@outlook.com>
2018-07-13 16:59:15 +0430

    hello

---
+++ 1/Hello.py
@@ -0,0 +1,16 @@
+#python --version 
+#run python helloworld.py 
+#command line :python 
+#exit command line : exit() 
+
+
+print("Hello, World!")
+print("Hello, World!")
+print("Hello, World!")
+
+
+#Python Indentations
+
+print("Hello, World!")
+print("Hello, World!")
+print("Hello, World!")
